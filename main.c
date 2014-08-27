3017232d44a03b9548403614d0bac87b21314769
Grazvidas <grazvidasb@gmail.com>
2014-08-27 22:09:09 +0300

    main routine

--- main.c
+++ main.c
@@ -12,16 +12,12 @@
 
 int main (void)
 {
-	UART_init(PROTO_receivedData);
-
-	DDRC |= 1<<PC5;
-	sei();
+	UART_registerReceiveCallback(PROTO_receivedData);
+	UART_init();
+	CONFIG_init();
 
 	UART_log("$ ");
-//	uint8_t val;
-//	CONFIG_setParam(CONFIG_PARAM_TIME_FORMAT, 1);
-//	CONFIG_getParam(CONFIG_PARAM_TIME_FORMAT, &val);
-//	UART_log("val: %d\n", val);
+
 	while (1)
 	{
 
